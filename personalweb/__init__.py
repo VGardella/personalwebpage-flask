e2c6d9e65da16019c3925c45c40698b54ac220ef
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-11-18 21:31:08 -0300

    Add app config and views

--- personalweb/__init__.py
+++ personalweb/__init__.py
@@ -3,3 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# App configuration
+
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
+
+# Import views and blueprints
+
+from personalweb.views import base
+
+app.register_blueprint(base)
