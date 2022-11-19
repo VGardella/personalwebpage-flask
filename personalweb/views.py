38d4f90648393c1485803e63c82e86857755d16e
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-11-18 21:46:54 -0300

    Create principal views

--- personalweb/views.py
+++ personalweb/views.py
@@ -3,3 +3,16 @@ from flask import render_template, Blueprint
 
 # Create Blueprint and views
 base = Blueprint('base', __name__)
+
+@base.route('/')
+@base.route('/home')
+def home():
+    return render_template('home.html')
+
+@base.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@base.route('/background')
+def background():
+    return render_template('background.html')
